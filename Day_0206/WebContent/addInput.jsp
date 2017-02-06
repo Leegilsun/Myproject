b691497260b9b6ffff76ce6321fcdaaee00262ce
LeeGilSun <LeeGilSun@DESKTOP-HFE5H3H>
2017-02-06 10:41:43 +0900

    addInput.jsp에서 파라미터를 보내고 AddProcServlet에서 계산 후 출력

---
+++ Day_0206/WebContent/addInput.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<form method="get" action="addProc.do">
+<input type="text" name="num1">
+<br><br>
+<input type="text" name="num2">
+<input type="submit" value="신청" /> <input type="reset" value="취소" />
+</form>
+</body>
+</html>
---
+++ Day_0206/src/servlet/AddProcServlet.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addProc.do")
+public class AddProcServlet extends HttpServlet{
+	
+	protected void doProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		int result = num1 + num2;
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = resp.getWriter();
+		pw.println("<HTML>");
+		pw.println("<head>");
+		pw.println("<title>");
+		pw.println("현재 시간이에요");
+		pw.println("</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("두 수 합은"+result);
+		pw.println("</body>");
+		pw.println("</HTML>");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		doProc(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
