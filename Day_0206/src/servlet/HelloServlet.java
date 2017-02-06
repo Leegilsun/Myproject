c08feff6c0b1f1b0f1dad41a9ba243e983ab58cb
LeeGilSun <LeeGilSun@DESKTOP-HFE5H3H>
2017-02-06 10:23:00 +0900

    기본적인 servlet

---
+++ Day_0206/src/servlet/HelloServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.print.PrinterJobWrapper;
+
+@WebServlet("/hello.do")
+public class HelloServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		
+		resp.setContentType("text/html;charset=utf-8");
+		//get방식의 요청이 오면 수행되는 메소드
+		PrintWriter pw = resp.getWriter();	//HTML코드 출력을 위한 객체
+		pw.println("<HTML>");
+		pw.println("<head>");
+		pw.println("<title>");
+		pw.println("제목이에요");
+		pw.println("</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h1>Hello World</h1>");
+		pw.println("</body>");
+		pw.println("</HTML>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doPost(req, resp);
+		//post방식의 요청이 오면 수행되는 메소드
+	}
+
+}
